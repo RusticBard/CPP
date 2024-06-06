75626651278d65675703eeba84be439dc644467f
RusticBard <sanket.p.kumbhar.2805@gmail.com>
2024-06-06 22:32:15 +0530

    Updated compile.sh so that the it can be ran from anywhere

--- 1.hello_cpp/compile.sh
+++ 1.hello_cpp/compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-g++ -std=c++20 ./*.cpp -o  rooster.out && ./rooster.out @Flags
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+g++ -std=c++20 $SCRIPT_DIR/*.cpp -o  rooster.out && $SCRIPT_DIR/rooster.out @Flags
